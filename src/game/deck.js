d1b2f9bf77e95b26dcacb503903541c3bfb1d363
wazyen <franbdn31@gmail.com>
2022-01-29 15:27:47 +0100

    started filling game stacks

--- src/game/deck.js
+++ src/game/deck.js
@@ -1,6 +1,15 @@
 var DECK = {};
 
 //ALL CARDS
+DECK.persons = {
+	"Antonio": { type: Card.TYPE_PERSON, name: "Antonio", gender: "M", visuals: {}, traits: ["Deportista", "Torpe" /* ... */]},
+	"Maria": { type: Card.TYPE_PERSON, name: "Maria", gender: "F", visuals: {}, traits: ["Rata de biblioteca", "Egoísta" /* ... */]} // ...
+};
+
 DECK.events = {
-	"death": { type: Card.TYPE_EVENT }
+	"Death": { type: Card.TYPE_EVENT, name : "Death", visuals: {}} // ...
+};
+
+DECK.goals = {
+	"Olimpiadas": { type: Card.TYPE_GOAL, name: "Olimpiadas", visuals: {}} // ...
 };
